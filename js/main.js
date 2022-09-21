3667881e170a082e2ebab42e5edeef4c2bd0569f
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-09-21 21:12:31 +0000

    DOM manipulation dynamic update work

--- js/main.js
+++ js/main.js
@@ -18,25 +18,25 @@ let pages = [
     [
         "Add both digits together to get a new number",
         "Next",
-        "Ex: 14 is 1 + 4 = 5 /n click next to proceed",
+        "Ex: 14 is 1 + 4 = 5 \n click next to proceed",
         "Reset"
     ],
     [
         "Subtract your new number from the original number",
         "Next",
-        "Ex: 14 - 5 = 9 /n click next to proceed",
+        "Ex: 14 - 5 = 9 \n click next to proceed",
         "Reset"
     ],
     [
         "LIST OF NUMBERS AND SYMBOLS",
         "Reveal",
-        "Find your new number /n Note the symbol beside the new number",
+        "Find your new number \n Note the symbol beside the new number",
         "Reset"
     ],
     [
         "NINE SYMBOL",
         "",
-        "Your symbol is: /n NINE SYMBOL",
+        "Your symbol is: \n NINE SYMBOL",
         "Reset"
     ]
 ];
@@ -63,19 +63,27 @@ function updateDisplay() {
 
     let btn1 = document.getElementById('btn1');
     btn1.innerText = stateObj.btn1;
+    if (state > 0) {
+        btn1.addEventListener("click", function() { updateState(state+1)});
+    };
 
     let helperText = document.getElementById('helperText');
     helperText.innerText = stateObj.helperText;
 
     let btn2 = document.getElementById('btn2');
     btn2.innerText = stateObj.btn2;
+    if (state==0) {
+        btn2.addEventListener("click", function() { updateState(1)});
+    };
 
 };
+
+updateState(0); // Initializes page
 
 //console.log(pages[state][0]);
 
-for (let i=0; i<=5; i++) {
-    updateState(i);
-    console.log(stateObj);
-};
+// for (let i=0; i<=5; i++) {
+//     updateState(i);
+//     console.log(stateObj);
+// };
 
