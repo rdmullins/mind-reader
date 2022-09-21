d5585e268b19bd4c0e9c1295a281b0bb64b15997
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-09-21 19:41:21 +0000

    Defined page content array

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Mind Reader
+// Roger Mullins
+
+let class = 0;
+let pages = [
+    [
+        "I can read your mind",
+        "",
+        "",
+        "Go"
+    ],
+    [
+        "Pick a number from 01 - 99",
+        "Next",
+        "when you have your number click next",
+        "Reset"
+    ],
+    [
+        "Add both digits together to get a new number",
+        "Next",
+        "Ex: 14 is 1 + 4 = 5 /n click next to proceed",
+        "Reset"
+    ],
+    [
+        "Subtract your new number from the original number",
+        "Next",
+        "Ex: 14 - 5 = 9 /n click next to proceed",
+        "Reset"
+    ],
+    [
+        "LIST OF NUMBERS AND SYMBOLS",
+        "Reveal",
+        "Find your new number /n Note the symbol beside the new number",
+        "Reset"
+    ],
+    [
+        "NINE SYMBOL",
+        "",
+        "Your symbol is: /n NINE SYMBOL",
+        "Reset"
+    ]
+];
